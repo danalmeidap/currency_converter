2fd62a9f2b58f9caba847ee7fa94ad4d313b076f
danalmeidap <dbatistadealmeida23@gmail.com>
2023-10-03 21:15:53 -0300

    router

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from routers import router
+
 
 app = FastAPI()
+app.include_router(router)
+
 
 @app.get("/hello-world")
 def hello_world():
     return {"msg": "Hello World"}
+
