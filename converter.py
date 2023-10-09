cd0eb7f556b79bd2a6632e9c47ed4ae4070ec913
danalmeidap <dbatistadealmeida23@gmail.com>
2023-10-09 19:35:38 -0300

    route and converter

--- converter.py
+++ converter.py
@@ -3,19 +3,21 @@ from os import getenv
 from fastapi import HTTPException, status
 from config import settings
 
-API_KEY = getenv("API_KEY")
+API_KEY:str = getenv("API_KEY")
 
 def sync_converter(from_currency:str, to_currency:str, price:float):
-    url = settings.api.url+f'from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
+    url:str = settings.api.url+f'from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
     
     try:
-        response = get(url)
+        response: str | bytes = get(url)
     except Exception as error:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Internal error on alpha vantange requests: {error}')
 
-    data = response.json()    
+    data: str | bytes = response.json()    
 
     if "Realtime Currency Exchange Rate" not in data:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Probably invalid currencies given')
     
-    return float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    exchange_rate: float = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+    return  price * exchange_rateP
--- routers.py
+++ routers.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 router = APIRouter(prefix='/converter')
 
-@router.get("/{from_currency}")
+@router.get("/{from_currency}", status_code= status.HTTP_200_OK)
 def converter(from_currency: str, to_currency:str, price:float):
     return {"msg": f"{from_currency}"}
