782f50bb3c3aa8eaaf62d7e09772607c6399739c
danalmeidap <dbatistadealmeida23@gmail.com>
2023-10-21 23:52:40 -0300

    Pep8 Adjustments

--- converter.py
+++ converter.py
@@ -2,43 +2,66 @@ from requests import get
 from os import getenv
 from fastapi import HTTPException, status
 from config import settings
-import aiohttp
+from aiohttp import ClientSession
 
-API_KEY:str = getenv('API_KEY')
+API_KEY: str = getenv("API_KEY")
 
-def sync_converter(from_currency:str, to_currency:str, price:float):
-    url:str = settings.api.url+f'from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
-    
+
+def sync_converter(from_currency: str, to_currency: str, price: float):
+    url: str = (
+        settings.api.url
+        + f"from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}"
+    )
+
     try:
         response: str | bytes = get(url)
     except Exception as error:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Internal error on alpha vantange requests: {error}')
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal error on alpha vantange requests: {error}",
+        )
 
-    data: str | bytes = response.json()    
+    data: str | bytes = response.json()
 
     if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Probably invalid currencies given')
-    
-    exchange_rate: float = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Probably invalid currencies given",
+        )
 
-    return  price * exchange_rate
+    exchange_rate: float = float(
+        data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+    )
+
+    return price * exchange_rate
 
 
+async def async_converter(from_currency: str, to_currency: str, price: float):
+    url: str = (
+        settings.api.url
+        + f"from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}"
+    )
 
-async def async_converter(from_currency:str, to_currency:str, price:float):
-    url:str = settings.api.url+f'from_currency={from_currency}&to_currency={to_currency}&apikey={API_KEY}'
-    
     try:
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession() as session:
             async with session.get(url) as response:
-                data: str | bytes = await response.json() 
+                data: str | bytes = await response.json()
     except Exception as error:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Internal error on alpha vantange requests: {error}')
-
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Internal error on alpha vantange requests: {error}",
+        )
 
     if "Realtime Currency Exchange Rate" not in data:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Probably invalid currencies given')
-    
-    exchange_rate: float = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Probably invalid currencies given",
+        )
 
-    return  price * exchange_rate
+    exchange_rate: float = float(
+        data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+    )
+
+    return price * exchange_rate
+
+
--- routers.py
+++ routers.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter, status
-from converter import sync_converter,async_converter
-from typing import List
 from asyncio import gather
+from typing import List
+
+from fastapi import APIRouter, status
+
+from converter import async_converter, sync_converter
 
 router = APIRouter(prefix="/converter")
 
-@router.get("/{from_currancy}", status_code= status.HTTP_200_OK)
-def converter(from_currency: str, to_currency:str, price:float):
-    to_currencies:List[str] = to_currency.split(',')
-    result:List[float] = [sync_converter(from_currency, currency, price) for currency  in to_currencies] 
+
+@router.get("/{from_currancy}", status_code=status.HTTP_200_OK)
+def converter(from_currency: str, to_currency: str, price: float):
+    to_currencies: List[str] = to_currency.split(",")
+    result: List[float] = [
+        sync_converter(from_currency, currency, price)
+        for currency in to_currencies
+    ]
 
     return result
 
 
-@router.get("/async/{from_currancy}", status_code= status.HTTP_200_OK)
-async def async_converter_router(from_currency: str, to_currency:str, price:float):
-    to_currencies:List[str] = to_currency.split(',')
-    result: List[float] = await gather(*[async_converter(from_currency, currency, price) for currency in to_currencies])
-    
+@router.get("/async/{from_currancy}", status_code=status.HTTP_200_OK)
+async def async_converter_router(
+    from_currency: str, to_currency: str, price: float
+):
+    to_currencies: List[str] = to_currency.split(",")
+    result: List[float] = await gather(
+        *[
+            async_converter(from_currency, currency, price)
+            for currency in to_currencies
+        ]
+    )
+
     return result
